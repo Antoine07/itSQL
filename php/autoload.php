3f772d896860b5dcffebf838ab7fe226a3dbeded
Antoine07 <antoine.lucsko@gmail.com>
2021-06-29 10:37:21 +0200

    autolaod

--- php/autoload.php
+++ php/autoload.php
@@ -1,6 +1,22 @@
 <?php
-spl_autoload_register(function ($class_name) {
+spl_autoload_register(function ($className) {
     
-    var_dump($class_name);
+    var_dump($className);
+
+    $fileName = $className . ".php"; // le nom de la classe c'est le nom du fichier
+
+    if(file_exists($fileName)){
+
+        // die($fileName);
+
+        require_once $fileName ;
+
+        return;
+    }
+
+    die;
 });
 
+
+$model = new Model() ;
+$post = new Post() ;
